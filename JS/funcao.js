0e29be7f34cad743b7a5d4ec4aab3d555f9fd978
KaelNeutro <32816984+KaelNeutro@users.noreply.github.com>
2020-04-03 21:33:11 -0300

    add Jquery external

--- JS/funcao.js
+++ JS/funcao.js
@@ -63,11 +63,6 @@ $(document).ready( function() {
 $(document).ready(function() {
   function limpa_formulário_cep() {
     // Limpa valores do formulário de cep.
-    $("#rua").val("");
-    $("#bairro").val("");
-    $("#cidade").val("");
-    $("#uf").val("");
-    $("#ibge").val("");
     $("#labelcepstart").val("");
     $("#labelcepend").val("");
     $("#cepstart").val("");
@@ -92,27 +87,57 @@ $(document).ready(function() {
         if(validacep.test(cep)) {
 
           //Preenche os campos com "..." enquanto consulta webservice.
-          $("#rua").val("...");
-          $("#bairro").val("...");
-          $("#cidade").val("...");
-          $("#uf").val("...");
-          $("#ibge").val("...");
           $("#labelcepstart").html("...");
 
           //Consulta o webservice viacep.com.br/
           $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
 
             if (!("erro" in dados)) {              //Atualiza os campos com os valores da consulta.
-              $("#rua").val(dados.logradouro);
-              $("#bairro").val(dados.bairro);
-              $("#cidade").val(dados.localidade);
-              $("#uf").val(dados.uf);
-              $("#ibge").val(dados.ibge);
+             ender = " " + dados.logradouro + ", " + dados.bairro + ", " + dados.localidade;
+             alert("- ". ender);
+             $("#labelcepstart").html(ender);
+              } //end if.
+              else {                                //CEP pesquisado não foi encontrado.
+                limpa_formulário_cep();
+                alert("CEP não encontrado.");
+              }
+            });
+        } //end if.
+        else {//cep é inválido.
+          limpa_formulário_cep();
+          alert("Formato de CEP inválido.");
+        }
+      } //end if.
+      else {//cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+      }
+    });
+    //CEP END
+  //Quando o campo cep perde o foco.
+  $("#cepend").blur(function() {
 
-              
-              ender = " " + dados.logradouro + ", " + dados.bairro + ", " + dados.localidade;
-              alert("- ". ender);
-              $("#labelcepstart").html(ender);
+    //Nova variável "cep" somente com dígitos.
+    var cep = $(this).val().replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+      var ender= "";
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+          //Preenche os campos com "..." enquanto consulta webservice.
+          $("#labelcepend").html("...");
+
+          //Consulta o webservice viacep.com.br/
+          $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+            if (!("erro" in dados)) {              //Atualiza os campos com os valores da consulta.
+             ender = " " + dados.logradouro + ", " + dados.bairro + ", " + dados.localidade;
+             $("#labelcepEND").html(ender);
               } //end if.
               else {                                //CEP pesquisado não foi encontrado.
                 limpa_formulário_cep();
--- PHP/calendar.php
+++ PHP/calendar.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <meta charset='utf-8' />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
   <!-- Metodos para uso do Full Calendar --> 
   <link href='../fullcalendar/packages/core/main.css' rel='stylesheet' />
@@ -17,12 +19,13 @@
 
   <!--JQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script> -->
 
 
 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
@@ -39,6 +42,7 @@
 
   <div id='calendar'></div>
 
+  <!-- FORMULARIO PARA CADASTRA EVENTO CONFORME O DIA -->
   <div class="modal fade" id="cadEvento" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
@@ -56,9 +60,7 @@
               <label class="col-sm-2 col-form-label">Título</label>
               <div class="col-sm-10">
                 <input type="text" name="title" class="form-control " id="title" placeholder="Título do evento" required="">
-                <div class="valid-feedback">
-                  OK!
-                </div>
+                
                 <div class="invalid-feedback">
                   Preenche o campo. Por favor!
                 </div>
@@ -69,9 +71,7 @@
               <label class="col-sm-2 col-form-label">Número de pessoas</label>
               <div class="col-sm-10">
                 <input type="number" name="qtd_pessoas" class="form-control" id="qtd_pessoas" placeholder="" min="1" required="">
-                <div class="valid-feedback">
-                  OK!
-                </div>
+                
                 <div class="invalid-feedback">
                   Preenche o campo. Por favor!
                 </div>
@@ -82,9 +82,7 @@
               <label class="col-sm-2 col-form-label">Quantidade de Onibus</label>
               <div class="col-sm-10">
                 <input type="number" name="qtd_onibus" class="form-control" id="qtd_onibus" placeholder="" min="1" disabled="">
-                <div class="valid-feedback">
-                  OK!
-                </div>
+                
                 <div class="invalid-feedback">
                   Preenche o campo. Por favor!
                 </div>
@@ -93,7 +91,7 @@
             <div class="form-group row">
               <label class="col-sm-2 col-form-label">Tipos de eventos</label>
               <div class="col-sm-10">
-                <select name="color" class="form-control" id="color">
+                <select name="color" class="form-control" id="color" required="">
                   <option value="">Selecione</option>     
                   <option style="color:#FFD700;" value="#FFD700">Escola</option>
                   <option style="color:#0071c5;" value="#0071c5">Prefeitura</option>
@@ -103,6 +101,10 @@
                   <option style="color:#436EEE;" value="#436EEE">Ação Social</option>
                   <option style="color:#A020F0;" value="#A020F0">Outros</option>
                 </select>
+                
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
               </div>
             </div>
             <!--  DATA E HORA DO EVENTOS -->
@@ -111,20 +113,36 @@
                 <div class="form-group">
                   <label class="" for="start">Data início </label>
                   <input type="date" name="start" class="form-control" id="start"  disabled="">
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
                 <div class="form-group">
                   <label class="" for="start">Horario</label>
-                  <input type="time" name="timestart" class=" form-control ">
+                  <input type="time" name="timestart" class=" form-control " required="">
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
               </div>
               <div class="form-group col">
                 <div class="form-group">
                   <label class="" for="end">Data fim</label>
-                  <input type="date" name="end" class="form-control" id="end"  onkeypress="">
+                  <input type="date" name="end" class="form-control" id="end"  onkeypress="" required="">
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
                 <div class="form-group">
                   <label class="" for="start">Horario</label>
-                  <input type="time" name="timeend" class=" form-control ">
+                  <input type="time" name="timeend" class=" form-control " required="">
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
               </div>
             </div>
@@ -133,15 +151,23 @@
               <div class="form-group col">
                 <div class="form-group">
                   <label class="" for="cepstart">CEP  </label>
-                  <input type="text" name="cepstart" class="form-control" id="cepstart"  >
+                  <input type="text" name="cepstart" class="form-control" id="cepstart" required="">
                   <label id="labelcepstart"> Endereço </label>
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
               </div>
               <div class="form-group col">
                 <div class="form-group">
                   <label class="" for="cepend">CEP  </label>
-                  <input type="text" name="cepend" class="form-control" id="cepend"  >
+                  <input type="text" name="cepend" class="form-control" id="cepend"  required="">
                   <label id="labelcepend"></label>
+                  
+                <div class="invalid-feedback">
+                  Preenche o campo. Por favor!
+                </div>
                 </div>
               </div>
             </div>
